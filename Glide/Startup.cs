8928c65a75e1bd707e3ace78d77d065ac52e0bc5
keithtmccartney <keithtmccartney@hotmail.com>
2017-01-26 22:12:46 +0000

    Add project files.

---
+++ Glide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Glide.Startup))]
+namespace Glide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
